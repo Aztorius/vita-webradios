dcc67a080b2636a5810445c8db4840b24a3d6705
William Bonnaventure <william.bonnaventure@gmail.com>
2025-08-16 15:12:08 +0200

    Add basic UI

--- src/main.cpp
+++ src/main.cpp
@@ -327,8 +327,9 @@ int main(void)
 
 	ImGui_ImplVitaGL_TouchUsage(true);
 	ImGui_ImplVitaGL_UseIndirectFrontTouch(false);
-	ImGui_ImplVitaGL_UseRearTouch(true);
+	ImGui_ImplVitaGL_UseRearTouch(false);
 	ImGui_ImplVitaGL_GamepadUsage(true);
+	ImGui_ImplVitaGL_MouseStickUsage(false);
 
 	// Get or write playlist
 	struct m3u_file *m3ufile = NULL;
@@ -408,51 +409,37 @@ int main(void)
  
 	// Main loop
 	bool done = false;
+	static bool show_app = false;
+	static ImGuiWindowFlags flags = (ImGuiWindowFlags)(ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
 	while (!done) {
 		ImGui_ImplVitaGL_NewFrame();
 
-		if (ImGui::BeginMainMenuBar()){
-			if (ImGui::BeginMenu("Debug")){
-				ImGui::EndMenu();
+		{
+			ImGui::GetIO().MouseDrawCursor = false;
+			
+    		ImGui::SetNextWindowPos(ImVec2(0, 0));
+	    	ImGui::SetNextWindowSize(ImVec2(960, 544));
+
+			if (ImGui::Begin("Example: Fullscreen window", &show_app, flags))
+			{
+				if (player.url && player.title && player.state == PLAYER_STATE_PLAYING) {
+					ImGui::Text("Playing %s from %s", player.title, player.url);
+				} else {
+					ImGui::Text("Standby");
+				}
+
+				ImGui::Separator();
+				m3u_entry *drawEntry = m3ufile->first_entry;
+				while (drawEntry) {
+					if (drawEntry->title) {
+						ImGui::Button(drawEntry->title, ImVec2(960, 30));
+					} else if (drawEntry->url) {
+						ImGui::Button(drawEntry->url, ImVec2(960, 30));
+					}
+					drawEntry = drawEntry->next;
+				}
+				ImGui::End();
 			}
-			ImGui::EndMainMenuBar();
-		}
-
-		// 1. Show a simple window.
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-		{
-			static float f = 0.0f;
-			static int counter = 0;
-			ImGui::Text("Hello, world!");						   // Display some text (you can use a format string too)
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);			// Edit 1 float using a slider from 0.0f to 1.0f	
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			ImGui::Checkbox("Demo Window", &show_demo_window);	  // Edit bools storing our windows open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			if (ImGui::Button("Button"))							// Buttons return true when clicked (NB: most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-			// 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
-
-		// 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-		if (show_demo_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-			ImGui::ShowDemoWindow(&show_demo_window);
 		}
 
 		ctrl_press = ctrl_peek;
