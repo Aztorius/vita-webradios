95101275f798f64dae10fca5931745a86e2d492c
William Bonnaventure <william.bonnaventure@gmail.com>
2025-09-05 21:42:20 +0200

    Move utils functions to utils.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include <psp2/sysmodule.h>
 
 #include "visualizer/neon_fft.hpp"
+#include "utils.hpp"
 
 extern "C" {
 #include "audio/mp3.h"
@@ -55,43 +56,6 @@ static struct player player;
 static int audio_mutex;
 static int visualizer_mutex;
 
-
-char *icy_parse_stream_title(char *data, int size)
-{
-	// Find "StreamTitle='XXX'" in data and replace data value with "XXX\0"
-	if (size <= 13) {
-		sceClibPrintf("ICY Metadata to short to find stream title\n");
-		return NULL;
-	}
-
-	char *title = (char*)malloc(size);
-	if (!title) {
-		sceClibPrintf("Cannot allocate title\n");
-		return NULL;
-	}
-
-	memcpy(title, data, size);
-	title[size-1] = '\0';
-
-	char *result_left = strstr(title, "StreamTitle='");
-	if (!result_left) {
-		sceClibPrintf("Cannot find \"StreamTitle='\"\n");
-		free(title);
-		return NULL;
-	}
-
-	char *result_right = strstr(title + 13, "'");
-	if (!result_right) {
-		sceClibPrintf("Cannot find \"'\"\n");
-		free(title);
-		return NULL;
-	}
-
-	int title_size = result_right - (result_left + 13) + 1;
-	memmove(title, result_left + 13, title_size);
-	title[title_size-1] = '\0';
-	return title;
-}
 
 int play_webradio(const char *url)
 {
@@ -431,35 +395,6 @@ int http_thread(unsigned int args, void *argp) {
 		sceKernelDelayThread(200000); // Delay for 200 ms
 	}
 
-	return 0;
-}
-
-int copyfile(const char *destfile, const char *srcfile)
-{
-	FILE *fout = fopen(destfile, "wb");
-	if (!fout) {
-		printf("Cannot create file %s\n", destfile);
-		return -1;
-	}
-
-	FILE *fin = fopen(srcfile, "rb");
-	if (!fin) {
-		printf("Cannot open file %s\n", srcfile);
-		return -1;
-	}
-
-	char buf[512];
-	int nread = 0;
-
-	do {
-		nread = fread(buf, 1, 512, fin);
-		if (nread > 0) {
-			fwrite(buf, 1, nread, fout);
-		}
-	} while (nread > 0);
-
-	fclose(fin);
-	fclose(fout);
 	return 0;
 }
 
---
+++ src/utils.hpp
@@ -0,0 +1,7 @@
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+int copyfile(const char *destfile, const char *srcfile);
+char *icy_parse_stream_title(char *data, int size);
+
+#endif
