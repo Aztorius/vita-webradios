76395faa27ff6d19d2a17771efa3cd85d1878625
William Bonnaventure <william.bonnaventure@gmail.com>
2026-02-15 08:55:53 +0100

    Use curl to allow HTTPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <strings.h>
 #include <math.h>
 
 #include <imgui_vita.h>
 #include <vitaGL.h>
+#include <curl/curl.h>
 
 #include <psp2/ctrl.h>
 #include <psp2/audiodec.h>
@@ -69,7 +71,11 @@ struct player {
 	const char *title; // The station name
 	char *song_title; // Song title
 	bool new_song_title;
-	audio_format audio_format;
+	audio_format audio_type;
+	bool icy_metadata_enabled;
+	int icy_metaint;
+	int icy_metadata_index;
+	int icy_part_length;
 	neon_fft_config *visualizer_config;
 };
 
@@ -77,365 +83,198 @@ static struct player player;
 static int audio_mutex;
 static int visualizer_mutex;
 
+#define STREAM_BUFFER_SIZE (256 * 1024)
 
-int play_webradio(const char *url)
+static unsigned char stream_buffer[STREAM_BUFFER_SIZE];
+static volatile int write_pos = 0;
+static volatile int read_pos = 0;
+
+
+int progress_callback(void *clientp,
+                      curl_off_t dltotal,
+                      curl_off_t dlnow,
+                      curl_off_t ultotal,
+                      curl_off_t ulnow)
 {
-	int res, tpl, conn, req;
-	unsigned long long length = 0;
-
-	char *responseHeaders;
-	unsigned int responseHeaderSize = 0;
-
-	const char *headerFieldValue = NULL;
-	unsigned int headerFieldSize = 0;
-
-	int icyMetaint = 0;
-	bool icyMetadataEnabled = false;
-	int nextIcyMetadataIndex = 0;
-	int icyMetadataPartLength = 0;
-
-	SceUID fd;
-	void *recv_buffer = NULL;
-
-	SceNetInitParam net_init_param;
-	net_init_param.size = 16 * 1024;
-	net_init_param.flags = 0;
-
-	SceUID memid = sceKernelAllocMemBlock("SceNetMemory", 0x0C20D060, net_init_param.size, NULL);
-	if(memid < 0){
-		sceClibPrintf("sceKernelAllocMemBlock failed (0x%X)\n", memid);
-		return memid;
+    if (player.state != PLAYER_STATE_PLAYING) {
+		// stop curl
+        return 1;
 	}
 
-	sceKernelGetMemBlockBase(memid, &net_init_param.memory);
-
-	res = sceNetInit(&net_init_param);
-	if(res < 0){
-		sceClibPrintf("sceNetInit failed (0x%X)\n", res);
-		goto free_memblk;
-	}
-
-	res = sceNetCtlInit();
-	if(res < 0){
-		sceClibPrintf("sceNetCtlInit failed (0x%X)\n", res);
-		goto net_term;
-	}
-
-	res = sceSslInit(300 * 1024);
-	if(res < 0){
-		sceClibPrintf("sceSslInit failed (0x%X)\n", res);
-		goto http_term;
-	}
-
-	res = sceHttpInit(40 * 1024);
-	if(res < 0){
-		sceClibPrintf("sceHttpInit failed (0x%X)\n", res);
-		goto netctl_term;
-	}
-
-	sceHttpsDisableOption(SCE_HTTPS_FLAG_SERVER_VERIFY);
-
-	tpl = sceHttpCreateTemplate("PSVita", SCE_HTTP_VERSION_1_1, SCE_TRUE);
-	if(tpl < 0){
-		sceClibPrintf("sceHttpCreateTemplate failed (0x%X)\n", tpl);
-		goto ssl_term;
-	}
-
-	res = sceHttpAddRequestHeader(tpl, "Accept", "audio/mpeg", SCE_HTTP_HEADER_ADD);
-	sceClibPrintf("sceHttpAddRequestHeader=0x%X\n", res);
-	// Accept Icy-Metadata for getting song title
-	res = sceHttpAddRequestHeader(tpl, "Icy-Metadata", "1", SCE_HTTP_HEADER_ADD);
-	sceClibPrintf("sceHttpAddRequestHeader=0x%X\n", res);
-
-	conn = sceHttpCreateConnectionWithURL(tpl, url, SCE_TRUE);
-	if(conn < 0){
-		sceClibPrintf("sceHttpCreateConnectionWithURL failed (0x%X)\n", conn);
-		goto http_del_temp;
-	}
-
-	req = sceHttpCreateRequestWithURL(conn, SCE_HTTP_METHOD_GET, url, 0);
-	if(req < 0){
-		sceClibPrintf("sceHttpCreateRequestWithURL failed (0x%X)\n", req);
-		goto http_del_conn;
-	}
-
-	res = sceHttpSendRequest(req, NULL, 0);
-	if(res < 0){
-		sceClibPrintf("sceHttpSendRequest failed (0x%X)\n", res);
-		goto http_del_req;
-	}
-
-	res = sceHttpGetAllResponseHeaders(req, &responseHeaders, &responseHeaderSize);
-	sceClibPrintf("sceHttpGetAllResponseHeaders=%i\n", res);
-	sceClibPrintf("responseHeaderSize=%i\n", responseHeaderSize);
-
-	res = sceHttpParseResponseHeader(responseHeaders, responseHeaderSize, "icy-metaint", &headerFieldValue, &headerFieldSize);
-	sceClibPrintf("sceHttpParseResponseHeader=%i\n", res);
-	if (res >= 0) {
-		icyMetaint = strtol(headerFieldValue, NULL, 10);
-		icyMetadataEnabled = true;
-		nextIcyMetadataIndex = icyMetaint;
-		sceClibPrintf("ICY metadata enabled with icy-metaint=%i\n", icyMetaint);
-	}
-
-	res = sceHttpParseResponseHeader(responseHeaders, responseHeaderSize, "Content-Type", &headerFieldValue, &headerFieldSize);
-	sceClibPrintf("sceHttpParseResponseHeader=%i\n", res);
-	if (res >= 0) {
-		sceClibPrintf("Content-Type: %s\n", headerFieldValue);
-		if (headerFieldValue == "audio/mpeg") {
-			player.audio_format = AUDIO_FORMAT_MP3;
-		} else if (headerFieldValue == "audio/aac" || headerFieldValue == "audio/aacp") {
-			player.audio_format = AUDIO_FORMAT_AAC;
-		} else if (headerFieldValue == "audio/ogg") {
-			player.audio_format = AUDIO_FORMAT_OGG;
-		} else {
-			// Use MP3 by default
-			player.audio_format = AUDIO_FORMAT_MP3;
-		}
-	} else {
-		// Use MP3 by default
-		player.audio_format = AUDIO_FORMAT_MP3;
-	}
-
-	res = sceHttpGetResponseContentLength(req, &length);
-	sceClibPrintf("sceHttpGetResponseContentLength=%i\n", length);
-
-	if (res < 0) {
-		recv_buffer = malloc(0x10000);
-		if (recv_buffer == NULL) {
-			sceClibPrintf("sce_paf_memalign return to NULL\n");
-			goto http_abort_req;
-		}
-
-		int ret = 0;
-		player.state = PLAYER_STATE_PLAYING;
-
-		while (player.state == PLAYER_STATE_PLAYING && player.url == url) {
-			res = sceHttpReadData(req, recv_buffer, 0x10000);
-			if (res <= 0) {
-				break;
-			}
-
-			if (sceKernelLockMutex(audio_mutex, 1, NULL) < 0) {
-				ret = 0;
-				printf("Cannot lock mutex in http thread, skipping audio\n");
-			} else {
-				if (icyMetadataEnabled) {
-					// We need to ignore those metadata before feeding it into the MP3 decoder
-					// Also we will decode those metadata to get the song title
-					if (nextIcyMetadataIndex > res) {
-						// We do not have metadata here
-						nextIcyMetadataIndex -= res;
-						ret = MP3_Feed(recv_buffer, res);
-						if (ret) {
-							printf("MP3_Feed error: %i\n", ret);
-							break;
-						}
-					} else {
-						if (nextIcyMetadataIndex > 0) {
-							// Feed buffer before metadata
-							ret = MP3_Feed(recv_buffer, nextIcyMetadataIndex);
-							if (ret) {
-								printf("MP3_Feed error: %i\n", ret);
-								break;
-							}
-						}
-
-						while (nextIcyMetadataIndex < res) {
-							// Parse first byte to get metadata size for this chunk
-							icyMetadataPartLength = (int)(*((char*)recv_buffer + nextIcyMetadataIndex)) * 16;
-							if (icyMetadataPartLength > 0) {
-								// We have some metadata to parse !
-								sceClibPrintf("icyMetadataPartLength=%i\n", icyMetadataPartLength);
-								if (player.song_title) {
-									free(player.song_title);
-									player.song_title = NULL;
-								}
-								if (nextIcyMetadataIndex + 1 + icyMetadataPartLength <= res) {
-									player.song_title = icy_parse_stream_title((char*)recv_buffer + nextIcyMetadataIndex + 1, icyMetadataPartLength);
-									if (player.song_title) {
-										player.new_song_title = true;
-										sceClibPrintf("Title: %s\n", player.song_title);
-									}
-								}
-							} else if (icyMetadataPartLength < 0) {
-								// This is some weird value, exit
-								break;
-							}
-
-							if (nextIcyMetadataIndex + 1 + icyMetadataPartLength + icyMetaint <= res) {
-								// Feed next data
-								ret = MP3_Feed((char*)recv_buffer + nextIcyMetadataIndex + 1 + icyMetadataPartLength, icyMetaint);
-							} else {
-								// Feed until the end of buffer
-								ret = MP3_Feed((char*)recv_buffer + nextIcyMetadataIndex + 1 + icyMetadataPartLength, res - (nextIcyMetadataIndex + 1 + icyMetadataPartLength));
-							}
-							if (ret) {
-								printf("MP3_Feed error: %i\n", ret);
-								break;
-							}
-
-							nextIcyMetadataIndex += 1 + icyMetadataPartLength + icyMetaint;
-						}
-						nextIcyMetadataIndex -= res;
-					}
-				} else {
-					ret = MP3_Feed(recv_buffer, res);
-				}
-
-				sceKernelUnlockMutex(audio_mutex, 1);
-			}
-			if (ret) {
-				printf("MP3_Feed error: %i\n", ret);
-				break;
-			}
-		}
-	} else {
-		sceClibPrintf("length=0x%llX\n", length);
-		printf("This is a fixed size request ?! Aborting");
-	}
-
-http_abort_req:
-	sceHttpAbortRequest(req);
-
-http_del_req:
-	sceHttpDeleteRequest(req);
-	req = -1;
-
-http_del_conn:
-	sceHttpDeleteConnection(conn);
-	conn = -1;
-
-http_del_temp:
-	sceHttpDeleteTemplate(tpl);
-	tpl = -1;
-
-ssl_term:
-	sceSslTerm();
-
-http_term:
-	sceHttpTerm();
-
-netctl_term:
-	sceNetCtlTerm();
-
-net_term:
-	sceNetTerm();
-
-free_memblk:
-	sceKernelFreeMemBlock(memid);
-	memid = -1;
-
-	free(recv_buffer);
-	recv_buffer = NULL;
-
-	return 0;
+    return 0;
 }
 
-int audio_thread(unsigned int args, void *argp) {
-	int port = -1;
+size_t stream_callback(void *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    size_t bytes = size * nmemb;
+    unsigned char *data = (unsigned char *)ptr;
 
-	int ret = 0;
+    for (size_t i = 0; i < bytes; i++) {
+        int next = (write_pos + 1) % STREAM_BUFFER_SIZE;
 
+		// Full buffer
+        if (next == read_pos)
+            break;
+
+        stream_buffer[write_pos] = data[i];
+        write_pos = next;
+    }
+
+    return bytes;
+}
+
+size_t header_callback(char *buffer, size_t size, size_t nitems, void *userdata)
+{
+    size_t len = size * nitems;
+
+    if (!strncasecmp(buffer, "icy-metaint:", 12)) {
+        int metaint = atoi(buffer + 12);
+        printf("ICY metaint = %d\n", metaint);
+
+		player.icy_metadata_enabled = true;
+		player.icy_metaint = metaint;
+		player.icy_metadata_index = metaint;
+    }
+
+    if (!strncasecmp(buffer, "content-type:", 13)) {
+		char content_type[30];
+		strncpy(content_type, buffer, 30);
+        printf("Content-Type = %.*s", (int)len, content_type);
+
+		if (!strcmp(content_type, "audio/mpeg")) {
+			player.audio_type = AUDIO_FORMAT_MP3;
+		} else if (!strcmp(content_type, "audio/acc") || !strcmp(content_type, "audio/aacp")) {
+			player.audio_type = AUDIO_FORMAT_AAC;
+		} else if (!strcmp(content_type, "audio/ogg")) {
+			player.audio_type = AUDIO_FORMAT_OGG;
+		} else {
+			// Suppose MP3
+			player.audio_type = AUDIO_FORMAT_MP3;
+		}
+    }
+
+    return len;
+}
+
+int network_thread(unsigned int args, void *argp)
+{
+    CURL *curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, player.url);
+
+	// Headers
+	struct curl_slist *headers = NULL;
+	headers = curl_slist_append(headers, "User-Agent: VitaWebradios/2.0");
+	headers = curl_slist_append(headers, "Icy-MetaData: 1");
+	headers = curl_slist_append(headers, "Accept: */*");
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+	// Headers callback
+	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
+	curl_easy_setopt(curl, CURLOPT_HEADERDATA, NULL);
+
+	// Stream callback
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, stream_callback);
+	curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, 16 * 1024);
+    curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+
+	// Progress callback
+	curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, progress_callback);
+	curl_easy_setopt(curl, CURLOPT_XFERINFODATA, NULL);
+	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+
+    // HTTPS (disable checks)
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+	player.state = PLAYER_STATE_PLAYING;
+
+    curl_easy_perform(curl);	// Blocking
+
+	curl_slist_free_all(headers);
+    curl_easy_cleanup(curl);
+
+    return 0;
+}
+
+#define AUDIO_CHUNK 4096
+
+int audio_thread(unsigned int args, void *argp)
+{
+    unsigned char audio_chunk[AUDIO_CHUNK] = {0};
 	unsigned char outbuffer[BUFFER_LENGTH] = {0};
 	unsigned int outsize = 0;
 	int channels = 0;
+	int port = -1;
 
-	while (player.state != PLAYER_STATE_STOPPING) {
-		if (sceKernelLockMutex(audio_mutex, 1, NULL) < 0) {
-			sceKernelDelayThread(100000); // Delay for 100 ms
-			printf("audio_thread: error locking mutex\n");
-			continue;
-		}
+    while (player.state != PLAYER_STATE_STOPPING) {
+        int count = 0;
 
-		if (player.state == PLAYER_STATE_WAITING || player.state == PLAYER_STATE_NEW) {
-			do {
-				// Consume everything
-				ret = MP3_Decode(NULL, 0, outbuffer, BUFFER_LENGTH, &outsize);
-			} while (!ret);
-			sceKernelUnlockMutex(audio_mutex, 1);
-			sceKernelDelayThread(100000); // Delay for 100 ms
-			continue;
-		}
+        while (read_pos != write_pos && count < AUDIO_CHUNK) {
+            audio_chunk[count++] = stream_buffer[read_pos];
+            read_pos = (read_pos + 1) % STREAM_BUFFER_SIZE;
+        }
 
-		ret = MP3_Decode(NULL, 0, outbuffer, BUFFER_LENGTH, &outsize);
-		if (ret == -11) {
-			// New format, close old output if necessary
-			if (port >= 0) {
-				sceAudioOutReleasePort(port);
-			}
+        if (count > 0) {
+			int ret = 0;
+			ret = MP3_Decode(audio_chunk, count, outbuffer, BUFFER_LENGTH, &outsize);
 
-			if (player.visualizer_config) {
-				sceKernelLockMutex(visualizer_mutex, 1, NULL);
-				neon_fft_free(player.visualizer_config);
-				player.visualizer_config = nullptr;
-				sceKernelUnlockMutex(visualizer_mutex, 1);
-			}
-	
-			channels = MP3_GetChannels();
-			int samplerate = MP3_GetSampleRate();
-			int nsamples = 0;
-			int compatible_freqs[] = {8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000};
-			int samplerate_is_compatible = false;
+			if (ret == -11) {
+				// New format, close old output if necessary
+				if (port >= 0) {
+					sceAudioOutReleasePort(port);
+				}
 
-			for (int i = 0; i < sizeof(compatible_freqs); i++) {
-				if (samplerate == compatible_freqs[i]) {
-					samplerate_is_compatible = true;
+				channels = MP3_GetChannels();
+				int samplerate = MP3_GetSampleRate();
+				int nsamples = 0;
+				int compatible_freqs[] = {8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000};
+				int samplerate_is_compatible = false;
+
+				for (int i = 0; i < sizeof(compatible_freqs); i++) {
+					if (samplerate == compatible_freqs[i]) {
+						samplerate_is_compatible = true;
+						break;
+					}
+				}
+
+				if (!samplerate_is_compatible) {
+					printf("Samplerate %i is not compatible\n", samplerate);
 					break;
+				}
+
+				SceAudioOutMode channels_mode = SCE_AUDIO_OUT_MODE_STEREO;
+				if (channels == 1) {
+					channels_mode = SCE_AUDIO_OUT_MODE_MONO;
+					nsamples = BUFFER_LENGTH >> 1; // 2 bytes per sample in mono mode
+				} else if (channels == 2) {
+					channels_mode = SCE_AUDIO_OUT_MODE_STEREO;
+					nsamples = BUFFER_LENGTH >> 2; // 4 bytes per sample in stereo mode (2x2)
+				} else {
+					printf("Wrong number of channel in stream !");
+					break;
+				}
+
+				port = sceAudioOutOpenPort(SCE_AUDIO_OUT_PORT_TYPE_BGM, nsamples, samplerate, channels_mode);
+				printf("Playing %s %s sample_rate %i channels %i\n", player.title, player.url, samplerate, channels);
+				sceAudioOutSetConfig(port, -1, -1, channels_mode);
+				SceAudioOutChannelFlag flags = (SceAudioOutChannelFlag)(SCE_AUDIO_VOLUME_FLAG_L_CH & SCE_AUDIO_VOLUME_FLAG_R_CH);
+				int vol = SCE_AUDIO_VOLUME_0DB;
+				int volumes[2] = {vol, vol};
+				if (sceAudioOutSetVolume(port, flags, volumes)) {
+					printf("Error setting volume\n");
 				}
 			}
 
-			if (!samplerate_is_compatible) {
-				printf("Samplerate %i is not compatible\n", samplerate);
-				goto audio_thread_end;
+			if (outsize > 0 && ret != -11) {
+				// Only play music if there is some music data
+				// Also ignore the first time we receive data (ret==-11) to prevent some bad noise
+				sceAudioOutOutput(port, outbuffer);
 			}
+        }
 
-			SceAudioOutMode channels_mode = SCE_AUDIO_OUT_MODE_STEREO;
-			if (channels == 1) {
-				channels_mode = SCE_AUDIO_OUT_MODE_MONO;
-				nsamples = BUFFER_LENGTH >> 1; // 2 bytes per sample in mono mode
-			} else if (channels == 2) {
-				channels_mode = SCE_AUDIO_OUT_MODE_STEREO;
-				nsamples = BUFFER_LENGTH >> 2; // 4 bytes per sample in stereo mode (2x2)
-			} else {
-				printf("Wrong number of channel in stream !");
-				goto audio_thread_end;
-			}
-
-			if (sceKernelLockMutex(visualizer_mutex, 1, NULL) >= 0) {
-				player.visualizer_config = neon_fft_init(nsamples, samplerate, channels, 16);
-				sceKernelUnlockMutex(visualizer_mutex, 1);
-			} else {
-				printf("visualizer_mutex lock error\n");
-			}
-	
-			port = sceAudioOutOpenPort(SCE_AUDIO_OUT_PORT_TYPE_BGM, nsamples, samplerate, channels_mode);
-			printf("Playing %s %s sample_rate %i channels %i\n", player.title, player.url, samplerate, channels);
-			sceAudioOutSetConfig(port, -1, -1, channels_mode);
-			SceAudioOutChannelFlag flags = (SceAudioOutChannelFlag)(SCE_AUDIO_VOLUME_FLAG_L_CH & SCE_AUDIO_VOLUME_FLAG_R_CH);
-			int vol = SCE_AUDIO_VOLUME_0DB;
-			int volumes[2] = {vol, vol};
-			if (sceAudioOutSetVolume(port, flags, volumes)) {
-				printf("Error setting volume\n");
-			}
-		}
-
-		sceKernelUnlockMutex(audio_mutex, 1);
-		
-		if (outsize > 0 && ret != -11) {
-			// Only play music if there is some music data
-			// Also ignore the first time we receive data (ret==-11) to prevent some bad noise
-			if (sceKernelLockMutex(visualizer_mutex, 1, NULL) >= 0) {
-				neon_fft_fill_buffer(player.visualizer_config, (int16_t*)outbuffer, outsize / (2 * channels)); // 2 bytes per sample
-				sceKernelUnlockMutex(visualizer_mutex, 1);
-			} else {
-				printf("visualizer_mutex lock error\n");
-			}
-			sceAudioOutOutput(port, outbuffer);
-		} else {
-			sceKernelDelayThread(100000); // Delay for 100 ms
-		}
-	}
+        sceKernelDelayThread(1000);
+    }
 
 audio_thread_end:
 	if (port >= 0) {
@@ -446,21 +285,6 @@ audio_thread_end:
 	return 0;
 }
 
-int http_thread(unsigned int args, void *argp) {
-	int playing = 0;
-	while (player.state != PLAYER_STATE_STOPPING)
-	{
-		if (!playing && player.state == PLAYER_STATE_NEW) {
-			playing = 1;
-			play_webradio(player.url);
-			playing = 0;
-		}
-
-		sceKernelDelayThread(200000); // Delay for 200 ms
-	}
-
-	return 0;
-}
 
 int main(void)
 {
@@ -504,6 +328,31 @@ int main(void)
 	sceSysmoduleLoadModule(SCE_SYSMODULE_NET);
 	sceSysmoduleLoadModule(SCE_SYSMODULE_HTTPS);
 
+	SceNetInitParam net_init_param;
+	net_init_param.size = 16 * 1024;
+	net_init_param.flags = 0;
+
+	SceUID memid = sceKernelAllocMemBlock("SceNetMemory", 0x0C20D060, net_init_param.size, NULL);
+	if(memid < 0){
+		sceClibPrintf("sceKernelAllocMemBlock failed (0x%X)\n", memid);
+		return memid;
+	}
+
+	sceKernelGetMemBlockBase(memid, &net_init_param.memory);
+
+	int res;
+	res = sceNetInit(&net_init_param);
+	if(res < 0){
+		sceClibPrintf("sceNetInit failed (0x%X)\n", res);
+		return 1;
+	}
+
+	res = sceNetCtlInit();
+	if(res < 0){
+		sceClibPrintf("sceNetCtlInit failed (0x%X)\n", res);
+		return 1;
+	}
+
 	audio_mutex = sceKernelCreateMutex("audioMutex", 0, 0, NULL);
 	if (audio_mutex < 0) {
 		printf("Error creating mutex\n");
@@ -525,7 +374,7 @@ int main(void)
 	SceCtrlData ctrl_peek, ctrl_press;
 
 	int thid = 0;
-	thid = sceKernelCreateThread("httpThread", http_thread, 0x10000100, 0x10000, 0, 0, NULL);
+	thid = sceKernelCreateThread("httpThread", network_thread, 0x10000100, 0x10000, 0, 0, NULL);
 	if (thid < 0) {
 		sceClibPrintf("Error creating thread with id %i\n", thid);
 		return 1;
@@ -783,6 +632,9 @@ int main(void)
 	// Cleanup
 	ImGui::DestroyContext();
 	glEnd();
+
+	sceNetCtlTerm();
+	sceNetTerm();
 
 	sceSysmoduleUnloadModule(SCE_SYSMODULE_HTTPS);
 	sceSysmoduleUnloadModule(SCE_SYSMODULE_NET);
